a8b40c1053c0964eed3754cc9fc33cb145b05a37
kenji yoshida <6b656e6a69@gmail.com>
2023-02-24 07:27:33 +0900

    Update Dialect.scala

--- common/src/main/scala/unused_proto/Dialect.scala
+++ common/src/main/scala/unused_proto/Dialect.scala
@@ -1,7 +1,7 @@
 package unused_proto
 
 /**
- * [[https://github.com/scalameta/scalameta/blob/v4.7.4/scalameta/dialects/shared/src/main/scala/scala/meta/Dialect.scala]]
+ * [[https://github.com/scalameta/scalameta/blob/v4.7.5/scalameta/dialects/shared/src/main/scala/scala/meta/Dialect.scala]]
  */
 sealed abstract class Dialect(val value: String) extends Product with Serializable
 
